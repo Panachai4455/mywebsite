b90e69d37a9e2e2d6ecee6621e0cc4f5f3f1c656
Panachai4455 <97937540+Panachai4455@users.noreply.github.com>
2022-02-27 13:19:04 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ไอจี</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>มาร์ค ลี</td>
+        <td>มาร์ค</td>
+        <td><img src="https://us-fbcloud.net/quiz/data/24/24166.qst0.ans1.jpg" width="130px"></td>
+        <td><a href="https://instagram.com/onyourm__ark?utm_medium=copy_link">instagram</a></td>
+      <tr>
+        <td>2</td>
+        <td>แฮชาน ลี</td>
+        <td>แฮชาน</td>
+        <td><img src="https://pbs.twimg.com/media/EkoG04YVMAA3O1y.jpg" width="130px"></td>
+        <td><a href="https://instagram.com/fullsun_ncit?utm_medium=copy_link">instagram</a></td>
+      <tr>
+        <td>3</td>
+        <td>ยองโฮ ซอ</td>
+        <td>จอห์นนี่</td>
+        <td><img src="https://i.pinimg.com/236x/80/77/1b/80771b2fbe417dab381480ba639e216a.jpg" width="130px"></td>
+        <td><a href="https://instagram.com/johnnyjsuh?utm_medium=copy_link">instagram</a></td>
+      <tr>
+        <td>4</td>
+        <td>แทอิล มูน</td>
+        <td>แทอิล</td>
+        <td><img src="https://i0.wp.com/dudeplace.co/wp-content/uploads/2021/07/notes_210706_225644_5328950296337329393623.jpg?fit=900%2C600&ssl=1" width="130px"></td>
+        <td><a href="https://instagram.com/mo.on_air?utm_medium=copy_link">instagram</a></td>
+      <tr>
+        <td>5</td>
+        <td>ยุนโอ จอง</td>
+        <td>แจฮยอน</td>
+        <td><img src="https://s.isanook.com/jo/0/rp/rc/w700h366/yacxacm1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2pvLzAvdWQvNDg0LzI0MjAwMDEvamFlaHl1bl9zbi5qcGc=.jpg" width="130px"></td>
+        <td><a href="https://instagram.com/_jeongjaehyun?utm_medium=copy_link">instagram</a></td>
+      <tr>
+        <td>6</td>
+        <td>ดงยอง คิม</td>
+        <td>โดยอง</td>
+        <td><img src="https://cms.dmpcdn.com/musicarticle/2020/12/17/0122d9a0-404e-11eb-9b6d-3fdf37c2e48e_original.png" width="130px"></td>
+        <td><a href="https://instagram.com/do0_nct?utm_medium=copy_link">instagram</a></td>
